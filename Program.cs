defcb395e9ab3cf029462246178ae9aa3824342d
LukaMkalavishvili <70640376+LukaMkalavishvili@users.noreply.github.com>
2020-09-03 13:13:32 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Transactions;
+
+namespace Firstproject
+{
+    class Program
+    {
+        static void Main()
+        {
+            System.Console.WriteLine("Hello!");
+            System.Console.WriteLine("My name is Luka Mkalavishvili");
+            System.Console.WriteLine("1");
+            System.Console.WriteLine("101");
+            System.Console.WriteLine("1001");
+            System.Console.WriteLine("Today is: " + DateTime.Now);
+            System.Console.WriteLine("Square root from 12345 is: " + Math.Sqrt(12345));
+            System.Console.WriteLine("Write your age bellow, please.");
+            var age = Console.ReadLine();
+            int ageConversion = Convert.ToInt32(age);
+            var additionalAge = 10;
+            var sum = ageConversion + additionalAge;
+            System.Console.WriteLine("Your age after 10 years will be : " + sum);
+            int counter = 0;
+            while(counter <= 100)
+            {
+                int evenNumbers = 2 + counter;
+                int oddNumbers = (2 + counter) * (-1);
+                if (counter % 2 == 0)
+                {
+                    System.Console.WriteLine(evenNumbers);
+                }
+                else
+                {
+                    System.Console.WriteLine(oddNumbers);
+                }
+                
+
+                counter++;
+            }
+        }
+    }
+}
